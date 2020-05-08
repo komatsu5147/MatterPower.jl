6071a4ca9c27cdcddd0ef1b0425d89b0c9bb22db
Eiichiro Komatsu <eiichirokomatsu@me.com>
2020-05-08 15:38:12 +0200

    Files generated by PkgTemplates

---
+++ src/MatterPower.jl
@@ -0,0 +1,5 @@
+module MatterPower
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MatterPower
+using Test
+
+@testset "MatterPower.jl" begin
+    # Write your own tests here.
+end
